c8a404a16e6693081d982f9bd16fb8067bbab2a2
minneee <alsgml022100@naver.com>
2022-08-08 23:06:05 +0900

    설정화면 테이블 뷰 생성

---
+++ Todo/Controller/SettingTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  SettingTableViewCell.swift
+//  Todo
+//
+//  Created by mini on 2022/08/08.
+//
+
+import UIKit
+
+class SettingTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- Todo/Controller/SettingViewController.swift
+++ Todo/Controller/SettingViewController.swift
@@ -10,16 +10,33 @@ import Alamofire
 
 class SettingViewController: UIViewController {
     
+    @IBOutlet weak var settingTableView: UITableView!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         self.navigationItem.title = "설정"
+        
+        self.settingTableView.delegate = self
+        self.settingTableView.dataSource = self
+        self.settingTableView.register(UINib(nibName: "SettingTableViewCell", bundle: nil), forCellReuseIdentifier: "SettingTableViewCell")
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         self.tabBarController?.tabBar.isHidden = true
     }
+}
+
+extension SettingViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let userCell = tableView.dequeueReusableCell(withIdentifier: "SettingTableViewCell", for: indexPath) as! SettingTableViewCell
+        return userCell
+    }
+    
+    
 }
